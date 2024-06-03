1c52f5757281483fbc6918d96056c7b54fcf1067
Paula <plnhschltz@gmail.com>
2024-06-03 19:01:35 -0300

    up

--- src/main/java/org/example/Biblioteca.java
+++ src/main/java/org/example/Biblioteca.java
@@ -1,4 +1,23 @@
-package org.example;
+import java.util.ArrayList;
 
 public class Biblioteca {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> livros = new ArrayList<>();
+
+
+        livros.add("1984 - George Orwell - 1949");
+        livros.add("To Kill a Mockingbird - Harper Lee - 1960");
+        livros.add("The Great Gatsby - F. Scott Fitzgerald - 1925");
+
+
+        for(int i = 0; i < livros.size(); i++) {
+            System.out.println(livros.get(i));
+        }
+
+
+        int index = livros.indexOf("The Great Gatsby - F. Scott Fitzgerald - 1925");
+        System.out.println("O índice do livro 'The Great Gatsby - F. Scott Fitzgerald - 1925' é: " + index);
+    }
 }
